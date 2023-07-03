552728862e9c302b07ab992284bd8ddf9a8c00d1
Andrew G <Andrew@andrews-imac.lan>
2023-07-03 11:24:48 -0400

    first

--- user-data.sh
+++ user-data.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
-export AWS_REGION=""
-export SQS_URL=""
+export AWS_REGION="us-east-1"
+export SQS_URL="sqs://"
 apt-get update -qq
 apt-get -qq -y install python3 python3-pip git
 curl -fsSL https://get.docker.com | bash -
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd /opt
-git clone https://github.com/geekrohit/celery-sqs-spot.git celery
+git clone https://github.com/GradientsDescend/sqs-celery-worker.git celery
 cd celery
 docker-compose up -d
