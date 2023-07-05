05363c2a910fd0eb9d337b6a7c51ca696e8da66d
Andrew G <Andrew@andrews-imac.lan>
2023-07-05 09:51:20 -0400

    change docker requrements and startup script

--- user-data.sh
+++ user-data.sh
@@ -3,9 +3,12 @@ export AWS_REGION="us-east-1"
 export SQS_URL="sqs://"
 apt-get update -qq
 apt-get -qq -y install python3 python3-pip git
-curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-chmod +x ./awslogs-agent-setup.py
-./awslogs-agent-setup.py -n -r us-east-1 -c s3://brewxml/cloudwatch.config
+#curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+#chmod +x ./awslogs-agent-setup.py
+#./awslogs-agent-setup.py -n -r us-east-1 -c s3://brewxml/cloudwatch.config
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:AmazonCloudWatch-linux
 curl -fsSL https://get.docker.com | bash -
 curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
